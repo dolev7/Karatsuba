6916fa6d8289d8cc5702eb365ebfb0ecfbef3409
dolev7 <d.dolev7@gmail.com>
2020-12-12 10:53:07 +0200

    vecotr changes #5

---
+++ Karatsuba/intArr.cpp
@@ -0,0 +1,84 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include "intArr.h"
+#include <string.h>
+#include <iostream>
+#include <string> 
+#include <vector>
+using namespace std;
+
+namespace Mult
+{
+    intArr::intArr(int size)
+    {
+        for (int i = 0; i < size; i++)
+            intArray.push_back(0);
+    }
+    intArr::~intArr()
+    {
+     //   delete[] intArray;
+    }
+    void intArr::insert(int place, int value)
+    {
+        intArray[place] = value;
+    }
+    void intArr::pushback(int value)
+    {
+        intArray.push_back(value);
+    }
+    void intArr::printArr()
+    {
+        for (int i = 0; i < static_cast<int>(intArray.size()); i++)
+            cout << intArray[i];
+    }
+    void intArr::shiftLeft(int toshift)
+    {
+        for (int i = 0; i < toshift; i++)
+            intArray.push_back(0);
+
+    }
+    int intArr::getActualSize()const
+    { 
+        int counter = 0;
+        bool leadZero = true;
+        for (int i = 0; i < static_cast<int>(intArray.size()); i++)
+        {
+            if (leadZero)
+            {
+                if (intArray[i] != 0)
+                    leadZero = false;
+            }
+            if (!leadZero)
+                counter++;
+        }
+        
+        return counter;
+    }
+
+    void intArr::resize()
+    {
+        int counter = 0;
+        bool leadZero = true;
+        for (int i = 0; i < static_cast<int>(intArray.size()); i++)
+        {
+            if (leadZero)
+            {
+                if (intArray[i] != 0)
+                    leadZero = false;
+            }
+            if (!leadZero)
+                counter++;
+        }
+        for (int i = 0; i < static_cast<int>(intArray.size() - counter); i++)
+            intArray[i] = intArray[i + counter];
+        intArray.resize(intArray.size() - counter);
+    }
+    void intArr::AddLeadingZeros(int numberOfZeros)
+    {
+        for (int i = 0; i < numberOfZeros; i++)
+        {
+            intArray.push_back(0);
+            rotate(intArray.rbegin(), intArray.rbegin() + 1, intArray.rend());
+        }
+    }
+
+};
